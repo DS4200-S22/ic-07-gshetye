5a28453891021b204bc7c1587a002478013e447a
Gary Shetye <garyshetye@Garys-MacBook-Pro.local>
2022-02-28 12:47:12 -0500

    Done

--- js/barchart.js
+++ js/barchart.js
@@ -1,22 +1,22 @@
 /*
 
 In-class activity 08 starter code
-Prof. Mosca 
-Modified: 12/08/21 
+Prof. Mosca
+Modified: 12/08/21
 
 */
 
-// Build your bar charts in this file 
+// Build your bar charts in this file
 
 
-// Set dimensions and margins for plots 
-const width = 900; 
-const height = 450; 
-const margin = {left:50, right:50, bottom:50, top:50}; 
-const yTooltipOffset = 15; 
+// Set dimensions and margins for plots
+const width = 900;
+const height = 450;
+const margin = {left:50, right:50, bottom:50, top:50};
+const yTooltipOffset = 15;
 
 
-// TODO: What does this code do? 
+// Adds an SVG in the hard-coded-bar div with a viewbox
 const svg1 = d3
   .select("#hard-coded-bar")
   .append("svg")
@@ -39,89 +39,144 @@ const data1 = [
 
   Axes
 
-*/ 
+*/
 
-// TODO: What does this code do? 
+// Gets the maximum value from the score
 let maxY1 = d3.max(data1, function(d) { return d.score; });
 
-// TODO: What does each line of this code do?   
+// Scales the y axis
 let yScale1 = d3.scaleLinear()
             .domain([0,maxY1])
-            .range([height-margin.bottom,margin.top]); 
+            .range([height-margin.bottom,margin.top]);
 
-// TODO: What does each line of this code do? 
+// Scales the x axis
 let xScale1 = d3.scaleBand()
             .domain(d3.range(data1.length))
             .range([margin.left, width - margin.right])
-            .padding(0.1); 
+            .padding(0.1);
 
-// TODO: What does each line of this code do?  
+// Adds the y axis to the page
 svg1.append("g")
-   .attr("transform", `translate(${margin.left}, 0)`) 
-   .call(d3.axisLeft(yScale1)) 
-   .attr("font-size", '20px'); 
+   .attr("transform", `translate(${margin.left}, 0)`)
+   .call(d3.axisLeft(yScale1))
+   .attr("font-size", '20px');
 
-// TODO: What does each line of this code do? 
+// Adds the x axis to the page
 svg1.append("g")
-    .attr("transform", `translate(0,${height - margin.bottom})`) 
-    .call(d3.axisBottom(xScale1) 
-            .tickFormat(i => data1[i].name))  
-    .attr("font-size", '20px'); 
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(xScale1)
+            .tickFormat(i => data1[i].name))
+    .attr("font-size", '20px');
 
-/* 
+/*
 
-  Tooltip Set-up  
+  Tooltip Set-up
 
 */
 
-// TODO: What does each line of this code do? 
-const tooltip1 = d3.select("#hard-coded-bar") 
-                .append("div") 
-                .attr('id', "tooltip1") 
-                .style("opacity", 0) 
-                .attr("class", "tooltip"); 
+// Adding a new div with class tooltip
+const tooltip1 = d3.select("#hard-coded-bar")
+                .append("div")
+                .attr('id', "tooltip1")
+                .style("opacity", 0)
+                .attr("class", "tooltip");
 
-// TODO: What does each line of this code do?  
+// Creates a function for adding text to the tooltip div
 const mouseover1 = function(event, d) {
-  tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
-          .style("opacity", 1);  
+  tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>")
+          .style("opacity", 1);
 }
 
-// TODO: What does each line of this code do? 
+// Creates a function that puts the position of the tooltip to the mouse's position
 const mousemove1 = function(event, d) {
-  tooltip1.style("left", (event.x)+"px") 
-          .style("top", (event.y + yTooltipOffset) +"px"); 
+  tooltip1.style("left", (event.pageX)+"px")
+          .style("top", (event.pageY + yTooltipOffset) +"px");
 }
 
-// TODO: What does this code do? 
-const mouseleave1 = function(event, d) { 
-  tooltip1.style("opacity", 0); 
+// Creates a function to make the tooltip disappear when the mouse leaves the bar
+const mouseleave1 = function(event, d) {
+  tooltip1.style("opacity", 0);
 }
 
-/* 
+/*
 
-  Bars 
+  Bars
 
 */
 
-// TODO: What does each line of this code do? 
-svg1.selectAll(".bar") 
-   .data(data1) 
-   .enter()  
-   .append("rect") 
-     .attr("class", "bar") 
-     .attr("x", (d,i) => xScale1(i)) 
-     .attr("y", (d) => yScale1(d.score)) 
-     .attr("height", (d) => (height - margin.bottom) - yScale1(d.score)) 
-     .attr("width", xScale1.bandwidth()) 
-     .on("mouseover", mouseover1) 
+// Creates all the bars in SVG1 with the bar class and sets its attributes and events
+svg1.selectAll(".bar")
+   .data(data1)
+   .enter()
+   .append("rect")
+     .attr("class", "bar")
+     .attr("x", (d,i) => xScale1(i))
+     .attr("y", (d) => yScale1(d.score))
+     .attr("height", (d) => (height - margin.bottom) - yScale1(d.score))
+     .attr("width", xScale1.bandwidth())
+     .on("mouseover", mouseover1)
      .on("mousemove", mousemove1)
      .on("mouseleave", mouseleave1);
 
 
+// Creates the second SVG in csv-bar for the second barchart
+
+const svg2 = d3
+  .select("#csv-bar")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
 
+// reads the csv and stores the data in data
+d3.csv("data/barchart.csv").then((data) => {
 
+  // d3.csv parses a csv file and passes the data
+  // to an anonymous function. Note how we build
+  // our visual inside of this anonymous function
 
+  // let's check our data
+  console.log(data);
 
+  // Gets the maximum value from the score
+  let maxY2 = d3.max(data, function(d) { return d.score; });
 
+  // Scales the y axis
+  let yScale2 = d3.scaleLinear()
+              .domain([0,maxY2])
+              .range([height-margin.bottom,margin.top]);
+
+  // Scales the x axis
+  let xScale2 = d3.scaleBand()
+              .domain(d3.range(data1.length))
+              .range([margin.left, width - margin.right])
+              .padding(0.1);
+
+  // Adds the y axis to the page
+  svg2.append("g")
+     .attr("transform", `translate(${margin.left}, 0)`)
+     .call(d3.axisLeft(yScale2))
+     .attr("font-size", '20px');
+
+  // Adds the x axis to the page
+  svg2.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(xScale2)
+              .tickFormat(i => data[i].name))
+      .attr("font-size", '20px');
+
+  // Creates all the bars in SVG2 with the bar class and sets its attributes and events
+  svg2.selectAll(".bar")
+     .data(data)
+     .enter()
+     .append("rect")
+       .attr("class", "bar")
+       .attr("x", (d,i) => xScale2(i))
+       .attr("y", (d) => yScale2(d.score))
+       .attr("height", (d) => (height - margin.bottom) - yScale2(d.score))
+       .attr("width", xScale2.bandwidth())
+       .on("mouseover", mouseover1)
+       .on("mousemove", mousemove1)
+       .on("mouseleave", mouseleave1);
+});
--- js/scatterplot.js
+++ js/scatterplot.js
@@ -1,17 +1,110 @@
 /*
 
 In-class activity 08 starter code
-Prof. Mosca 
-Modified: 12/08/21 
+Prof. Mosca
+Modified: 12/08/21
 
 */
 
-// Build your scatterplot in this file 
+// Build your scatterplot in this file
 
+// Creates the  SVG in csv-bar for the second barchart
+const svg3 = d3
+  .select("#csv-scatter")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
 
+// To do this, we'll need a python simple server
+// (python3 -m http.server) running from the directory that
+// our code is in.
 
+d3.csv("data/scatter.csv").then((data) => {
 
+  // d3.csv parses a csv file and passes the data
+  // to an anonymous function. Note how we build
+  // our visual inside of this anonymous function
 
+  // let's check our data
+  console.log(data);
 
+  // Our first step to mapping data to pixels is to find the
+  //  ranges for our x and y values
 
+  // find max X
+  let maxX3 = d3.max(data, (d) => { return d.day; });
+  console.log("Max x: " + maxX3);
 
+  // find max Y
+  let maxY3 = d3.max(data, (d) => { return d.score; });
+  console.log("Max y: " + maxY3);
+
+  // Now that we have our maxes we define scale functions that
+  // map our data values (domain for the scale function) to our
+  // pixel values (range for the scale function)
+
+  let xScale3 = d3.scaleLinear() // linear scale because we have
+                                // linear data
+                  .domain([0, maxX3])  // inputs for the function
+                  .range([margin.left, width - margin.right]);
+                  // ^ outputs for the function
+
+  let yScale3 = d3.scaleLinear()
+              .domain([0, maxY3])
+              .range([height - margin.bottom, margin.top]);
+
+// Adding a new div with class tooltip
+const tooltip2 = d3.select("#csv-scatter")
+                  .append("div")
+                  .attr('id', "tooltip2")
+                  .style("opacity", 0)
+                  .attr("class", "tooltip");
+
+// Creates a function for adding text to the tooltip div
+const mouseover2 = function(event, d) {
+    tooltip2.html("Day: " + d.day + "<br> Score: " + d.score + "<br>")
+            .style("opacity", 1);
+              }
+
+// Creates a function that puts the position of the tooltip to the mouse's position
+const mousemove2 = function(event, d) {
+    tooltip2.style("left", (event.pageX)+"px")
+            .style("top", (event.pageY + yTooltipOffset) +"px");
+              }
+
+// Creates a function to make the tooltip disappear when the mouse leaves the bar
+const mouseleave2 = function(event, d) {
+    tooltip2.style("opacity", 0);
+              }
+
+// Add x axis to svg6
+svg3.append("g") // g is a "placeholder" svg
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+// ^ moves axis to bottom of svg
+      .call(d3.axisBottom(xScale3)) // built in function for bottom
+// axis given a scale function
+      .attr("font-size", '20px'); // set font size
+
+// Add y axis to svg6
+svg3.append("g") // g is a "placeholder" svg
+      .attr("transform", `translate(${margin.left}, 0)`)
+// ^ move axis inside of left margin
+      .call(d3.axisLeft(yScale3)) // built in function for left
+// axis given a scale function
+      .attr("font-size", '20px'); // set font size
+
+svg3.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+          .attr("cx", (d) => xScale3(d.day)) // use xScale to return
+// pixel value for given datum
+          .attr("cy", (d) => yScale3(d.score)) // use yScale to return
+// pixel value for given datum
+          .attr("r", 10)
+          .attr("class", "circle")
+          .on("mouseover", mouseover2)
+          .on("mousemove", mousemove2)
+          .on("mouseleave", mouseleave2);
+});
